dba95c51132e684933d07d402f4d9e17737cd7e3
kristina15 <krisy-15012001@yandex.ru>
2021-02-18 02:26:45 +0400

    task 1

---
+++ Task 1/Task 1.2/Epam.Task1.2.3.LOWERCASE/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Epam.Task1._2._3.LOWERCASE
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.Write("ВВОД: ");
+            string str = Console.ReadLine();
+
+            string[] masStr = str.Split(new char[] { ' ', ',', '.', '?', '/', '-', ':', ';', '!' }, StringSplitOptions.RemoveEmptyEntries);
+            int counter = 0;
+            foreach (var item in masStr)
+            {
+                if (char.IsLower(item[0]))
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine($"ВЫВОД: {counter}");
+            
+        }
+    }
+}
---
+++ Task 1/Task 1.2/Epam.Task1.2.4.VALIDATOR/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task1._2._4.VALIDATOR
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.Write("ВВОД: ");
+            string str = Console.ReadLine();
+
+            string[] masStr = str.Split(new char[] { '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder builder = new StringBuilder();
+            foreach (var item in masStr)
+            {
+                if(char.IsLower(item[0]))
+                {
+                    builder[0]=char.ToUpper(item[0]);
+                }
+            }
+            string.Join(str, masStr);
+            Console.WriteLine($"ВЫВОД: {str}"); 
+        }
+    }
+}
