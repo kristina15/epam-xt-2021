10740b182694bc7a49475e248c14cfd371737369
kristina15 <krisy-15012001@yandex.ru>
2021-02-22 17:10:25 +0400

    Correct task

--- Task 1/Task 1.1/Epam.Task5.SUM_OF_NUMBERS/Program.cs
+++ Task 1/Task 1.1/Epam.Task5.SUM_OF_NUMBERS/Program.cs
@@ -6,16 +6,7 @@ namespace Epam.Task5.SUM_OF_NUMBERS
     {
         private static void Main()
         {
-            int sum = 8;
-            for (int i = 6; i < 1000; i++)
-            {
-                if (i % 15 == 0 || i % 3 == 0 || i % 5 == 0)
-                {
-                    sum += i;
-                }
-            }
-
-            Console.WriteLine($"sum={sum}");
+            Console.WriteLine(((3 + 999) / 2) * 333 + ((5 + 995) / 2) * 199 - (15 + 990) * 33);
         }
     }
 }
