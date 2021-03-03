42d78e650c5fd5dcdd96a2c22f84310dfba22a7c
kristina15 <krisy-15012001@yandex.ru>
2021-03-03 23:04:21 +0400

    correct task

--- Task 1/Task 1.2/Epam.Task1.2.4.VALIDATOR/Program.cs
+++ Task 1/Task 1.2/Epam.Task1.2.4.VALIDATOR/Program.cs
@@ -10,27 +10,48 @@ namespace Epam.Task1._2._4.VALIDATOR
             Console.Write("ВВОД: ");
             string str = Console.ReadLine();
 
-            string[] masStr = str.Split(new char[] { '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] masStr = GetArray(str);
             StringBuilder builder = new StringBuilder();
             int index = 0;
             foreach (var item in masStr)
             {
-                builder.Append(item);
-                for (int i = 0; i < item.Length; i++)
+                if (!string.IsNullOrEmpty(item))
                 {
-                    if(char.IsLetter(item[i]) && char.IsLower(item[i]))
+                    builder.Append(item);
+                    for (int i = 0; i < item.Length; i++)
                     {
-                        builder[index+i] = char.ToUpper(item[i]);
-                        break;
+                        if (char.IsLetter(item[i]) && char.IsLower(item[i]))
+                        {
+                            builder[index + i] = char.ToUpper(item[i]);
+                            break;
+                        }
                     }
-                }
 
-                index += item.Length;
-                builder.Append(str[index]);
-                index++;
+                    index += item.Length;
+                    builder.Append(str[index]);
+                    index++;
+                }
             }
 
             Console.WriteLine($"ВЫВОД: {builder}");
+        }
+
+        private static string[] GetArray(string str)
+        {
+            string[] array = new string[(str.Length / 2) + 1];
+            int k = 0;
+            int j = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i]=='?' || str[i] == '!' || str[i] == '.')
+                {
+                    array[j] = str.Substring(k, i - k);
+                    k = i + 1;
+                    j++;
+                }
+            }
+
+            return array;
         }
     }
 }
