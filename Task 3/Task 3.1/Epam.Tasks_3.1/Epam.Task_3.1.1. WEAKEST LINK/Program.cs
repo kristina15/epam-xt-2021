07c300edfa2a7918accfc800f4a340bb0661bef6
kristina15 <krisy-15012001@yandex.ru>
2021-03-21 22:48:37 +0400

    first version

---
+++ Task 3/Task 3.1/Epam.Tasks_3.1/Epam.Task_3.1.1. WEAKEST LINK/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Epam.Task_3._1._1._WEAKEST_LINK
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var queue = new Queue<int>();
+            Console.WriteLine("ВЫВОД: Введите N");
+            int numberOfPeople = GetNumberOfPeople();
+            Console.WriteLine("ВЫВОД: Сгенерирован круг людей. Начинаем вычеркивать каждого второго.");
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                queue.Enqueue(i + 1);
+            }
+
+            CrossOutEverySecond(queue);
+
+            Console.WriteLine($"Остался человек с номером: {queue.Peek()}");
+            Console.WriteLine("ВЫВОД: Игра окончена. Невозможно вычеркнуть больше людей.");
+        }
+
+        private static void CrossOutEverySecond(Queue<int> queue)
+        {
+            int round = 1;
+            while (queue.Count > 1)
+            {
+                queue.Enqueue(queue.Dequeue());
+                queue.Dequeue();
+                Console.WriteLine($"ВЫВОД: Раунд {round}. Вычеркнут человек. Людей осталось: {queue.Count}");
+                round++;
+            }
+        }
+
+        private static int GetNumberOfPeople()
+        {
+            int inputNumberOfPeople;
+            bool flag;
+            do
+            {
+                Console.Write("ВЫВОД: ");
+                flag = int.TryParse(Console.ReadLine(), out inputNumberOfPeople);
+            } while (!flag);
+
+            return inputNumberOfPeople;
+        }
+    }
+}
---
+++ Task 3/Task 3.1/Epam.Tasks_3.1/Epam.Task_3.1.2. TEXT ANALYSIS/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Epam.Task_3._1._2.TEXT_ANALYSIS
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Dictionary<string, int> pairs = new Dictionary<string, int>(StringComparer.CurrentCultureIgnoreCase);
+            string[] s = "I’m Greg. I’m nine. I hAVE got a nice red cat. It can jump and run. But I haven’t got a Greg dog, and I’m sad. This is Rob. He is three. He has got a little yellow fish. The fish is nice. It can swim. Rob is happy. This is Jillian. She has got four little kittens. Jillian is happy, too.".Split(new char[] { ' ', '.', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string item in s)
+            {
+                if (pairs.ContainsKey(item))
+                {
+                    pairs[item]++;
+                }
+                else
+                {
+                    pairs.Add(item, 1);
+                }
+            }
+
+            foreach (var item in pairs)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+        }
+    }
+}
