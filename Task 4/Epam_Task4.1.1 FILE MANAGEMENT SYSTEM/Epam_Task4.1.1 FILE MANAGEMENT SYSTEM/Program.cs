f1d24347a35071273fb01589af55bb57a95f17a2
kristina15 <krisy-15012001@yandex.ru>
2021-04-22 00:00:14 +0400

    first version

---
+++ Task 4/Epam_Task4.1.1 FILE MANAGEMENT SYSTEM/Epam_Task4.1.1 FILE MANAGEMENT SYSTEM/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Epam_Task4._1._1_FILE_MANAGEMENT_SYSTEM
+{
+    class Propgram
+    {
+        static void ShowStr(string st)
+        {
+            Console.WriteLine(st);
+        }
+
+        private static void Main()
+        {
+            Directory.CreateDirectory(ReserveObject._pathDict);
+            Console.WriteLine("Select the program mode\r\n1.The observer mode\r\n2.Mode for undoing changes\r\nTo exit the program, click any button");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Observer mode enabled");
+                    var reverser = new ReserveObject();
+                    reverser.CurEvents += ShowStr;
+                    Console.WriteLine("Press 'q' to quit the sample.");
+                    while (Console.Read() != 'q')
+                    {
+                    }
+                    break;
+                case "2":
+                    ReserveObject.RollBackChanges();
+                    break;
+                default:
+                    return;
+            }
+        }
+    }
+}
---
+++ Task 4/Epam_Task4.1.1 FILE MANAGEMENT SYSTEM/Epam_Task4.1.1 FILE MANAGEMENT SYSTEM/ReserveObject.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace Epam_Task4._1._1_FILE_MANAGEMENT_SYSTEM
+{
+    public delegate void delShow(string Message);
+
+    public delegate void delCheckDict();
+
+    class ReserveObject
+    {
+        public static readonly string _pathDict = Path.Combine(Directory.GetCurrentDirectory(), "MyTextFile");
+
+        public static readonly string _pathSystem = Path.Combine(Directory.GetCurrentDirectory(), "MyTextFileCopy");
+
+        readonly Dictionary<string, DateTime> _pathsAndDates = new Dictionary<string, DateTime>();
+
+        public event delShow CurEvents;
+
+        private readonly delCheckDict ChDict;
+
+        public ReserveObject()
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher
+            {
+                Path = _pathDict,
+                NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.LastAccess | NotifyFilters.FileName | NotifyFilters.DirectoryName | NotifyFilters.Size,
+                EnableRaisingEvents = true,
+                IncludeSubdirectories = true,
+                Filter = "*.*"
+            };
+
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnDelete);
+            watcher.Renamed += new RenamedEventHandler(OnRename);
+            watcher.IncludeSubdirectories = true;
+            ChDict = new delCheckDict(CheckDict);
+            IAsyncResult ir = ChDict.BeginInvoke(null, null);
+
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnRename(object sender, RenamedEventArgs e)
+        {
+            CurEvents($"Переименование файла {e.OldName} в {e.Name}");
+            if (e.Name.Contains(".txt"))
+            {
+                using (var sw = new StreamWriter($"{_pathSystem}\\LogFile.txt", true, Encoding.Default))
+                {
+                    sw.WriteLine($"{DateTime.Now}|Rename|{e.OldFullPath}|{CopyFile(e.FullPath, _pathSystem)}|");
+                }
+            }
+        }
+
+        private void OnDelete(object sender, FileSystemEventArgs e)
+        {
+            CurEvents($"Удаление файла {e.Name}");
+            using (var sw = new StreamWriter($"{_pathSystem}\\LogFile.txt", true, Encoding.Default))
+            {
+                sw.WriteLine($"{DateTime.Now}|Delete|{e.FullPath}||");
+            }
+        }
+
+        private void OnChanged(Object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+
+            lock (_pathsAndDates)
+            {
+                if (_pathsAndDates.ContainsKey(e.FullPath))
+                {
+                    _pathsAndDates.Remove(e.FullPath);
+                }
+                else
+                {
+                    _pathsAndDates.Add(e.FullPath, DateTime.Now);
+                }
+            }
+
+            MakeEvents(e.FullPath);
+        }
+
+        private void MakeEvents(string filePath)
+        {
+            if (filePath.Contains(".txt"))
+            {
+                CurEvents($"Изменение файла {filePath}");
+                try
+                {
+                    using (var sw = new StreamWriter($"{_pathSystem}\\LogFile.txt", true, Encoding.Default))
+                    {
+                        sw.WriteLine($"{DateTime.Now}|Change|{filePath}|{CopyFile(filePath, _pathSystem)}|");
+                    }
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        private static string CopyFile(string originalFileName, string newDirName)
+        {
+            string newFileName = $"{newDirName}\\{Path.GetFileName(originalFileName)}";
+            int numOfCopy = 0;
+            while (File.Exists(newFileName))
+            {
+                numOfCopy++;
+                newFileName = $"{newDirName}\\{Path.GetFileNameWithoutExtension(originalFileName)}({numOfCopy}){Path.GetExtension(originalFileName)}";
+            }
+
+            File.Copy(originalFileName, newFileName);
+            return newFileName;
+        }
+
+        private void CheckDict()
+        {
+            while (true)
+            {
+                foreach (KeyValuePair<string, DateTime> entry in _pathsAndDates)
+                {
+                    if ((DateTime.Now.Millisecond - entry.Value.Millisecond) > 2000)
+                    {
+                        MakeEvents(entry.Key);
+                        _pathsAndDates.Remove(entry.Key);
+                    }
+                }
+
+                Thread.Sleep(2000);
+            }
+        }
+
+        private static DateTime InputDate()
+        {
+            DateTime rollBackDate;
+            string input;
+            do
+            {
+                Console.WriteLine("Enter the rollback date in the format dd.MM.yyyy hh:mm:ss");
+                input = Console.ReadLine();
+            }
+            while (!DateTime.TryParseExact(input, "dd.MM.yyyy HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out rollBackDate));
+
+            return rollBackDate;
+        }
+
+        public static void RollBackChanges()
+        {
+            Directory.Delete(_pathDict, true);
+            Directory.CreateDirectory(_pathDict);
+            SearchingFiles(InputDate());
+        }
+
+        private static void SearchingFiles(DateTime creat)
+        {
+            var allText = File.ReadAllText($"{_pathSystem}\\LogFile.txt", Encoding.Default).Split('\n');
+            for (int i = 0; i < allText.Length; i++)
+            {
+                if (string.IsNullOrEmpty(allText[i]))
+                {
+                    var fileInfo = allText[i].Split(new char[] { '|' });
+                    bool flag = DateTime.TryParse(fileInfo[0], out DateTime fileDate);
+                    if (flag && fileDate <= creat)
+                    {
+                        switch (fileInfo[1])
+                        {
+                            case "Change":
+                                ChangeInfo(fileInfo[2], fileInfo[3]);
+                                break;
+
+                            case "Delete":
+                                DeleteInfo(fileInfo[2]);
+                                break;
+
+                            case "Rename":
+                                RenameInfo(fileInfo[2], fileInfo[3]);
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void RenameInfo(string originalName, string newName)
+        {
+            if (File.Exists(originalName))
+            {
+                File.Delete(Path.GetFullPath(originalName));
+            }
+
+            try
+            {
+                File.Copy(Path.GetFullPath(newName), $"{Path.GetDirectoryName(originalName)}\\{Path.GetFileName(newName)}", true);
+            }
+            catch
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(originalName));
+                File.Copy(Path.GetFullPath(newName), $"{Path.GetDirectoryName(originalName)}\\{Path.GetFileName(newName)}", true);
+            }
+        }
+
+        private static void DeleteInfo(string filePath)
+        {
+            try
+            {
+                File.Delete(Path.GetFullPath(filePath));
+            }
+            catch
+            {
+                Directory.Delete(Path.GetFullPath(filePath), true);
+            }
+        }
+
+        private static void ChangeInfo(string originalFilePath, string pathOfCopy)
+        {
+            try
+            {
+                File.Copy(Path.GetFullPath(pathOfCopy), Path.GetFullPath(originalFilePath), true);
+            }
+            catch
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(originalFilePath));
+                File.Copy(Path.GetFullPath(pathOfCopy), Path.GetFullPath(originalFilePath), true);
+            }
+        }
+    }
+}
